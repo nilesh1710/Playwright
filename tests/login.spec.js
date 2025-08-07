e7fe564265ef4fef3d36f9fb9d867465c5673594
Nilesh <nshekdar@tiuconsulting.com>
2025-08-07 13:12:36 +0530

    Update login test and CI workflow

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 
 test('test', async ({ page }) => {
+  //Login 
  await page.goto('https://surecafe.tiuconsulting.us/frontend/auth/login');
  await page.locator('[name="username"]').fill('ats434@yopmail.com');
 await page.locator('[name="password"]').fill('surecafe');
