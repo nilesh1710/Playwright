53856921a7a465b4eb78a0fe7f10693b6fdb5da2
Nilesh <nshekdar@tiuconsulting.com>
2025-08-08 16:32:18 +0530

    Search job  test and CI workflow

--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,16 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('login, navigate to SM, and logout', async ({ page }) => {
-  // Go to login page
   await page.goto('https://surecafe.tiuconsulting.us/frontend/auth/login', { waitUntil: 'networkidle' });
 
-  // Fill login form
    await page.goto('https://surecafe.tiuconsulting.us/frontend/auth/login');
  await page.locator('[name="username"]').fill('ats434@yopmail.com');
 await page.locator('[name="password"]').fill('surecafe');
   await page.getByRole('button', { name: 'Login' }).click();
 
-  // Click login button and wait for navigation to complete
  await Promise.all([
   page.waitForNavigation({ waitUntil: 'networkidle' }),
   page.evaluate(() => {
@@ -19,10 +16,8 @@ await page.locator('[name="password"]').fill('surecafe');
   }),
 ]);
 
-// Optional wait for UI to update
 await page.waitForTimeout(2000);
 
-// Try flexible locator for logout
 const logoutLink = page.locator('text=Log out');
 await page.evaluate(() => {
   const logoutLink = Array.from(document.querySelectorAll('a')).find(link => link.textContent.trim() === 'Log out');
